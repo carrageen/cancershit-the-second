cbd856043a54af856a6ede30e792c0f0d7512300
Schwopo <lofskim@gmail.com>
2017-04-09 14:06:18 +0200

    geiler comment boi

--- clientexample/APIExample.java
+++ clientexample/APIExample.java
@@ -11,7 +11,9 @@ public class APIExample {
 		//Ein CancerClient Object Verbindet sich mit dem Server und erstellt automatisch
 		//Den Thread um einkommende Nachrichten zu behandeln.
 		//CancerClient ist abstract und muss mit einer onMessageIncoming() Methode
-		//ausgestattet werden.
+		//ausgestattet werden die immer dann aufgerufen wird, wenn eine Nachricht eintrifft.
+		//Der Parameter ist der Text der Nachricht. In der Form "Name: Inhalt"
+		//Der Client wird nur mit Strings benutzt, Message Objecte kommen nicht vor.
 		
 		Client client = new Client();
 		client.connect(socket);
